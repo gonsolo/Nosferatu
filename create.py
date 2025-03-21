92ce7179c457b964377a6494eeabbba45aadb4c1
Gon Solo <gonsolo@gmail.com>
2025-03-21 02:24:49 +0100

    Play four times.

--- create.py
+++ create.py
@@ -3,13 +3,18 @@ file = open("nosferatu.ly", "w")
 notes = "\\version \"2.24.4\"\n"
 notes += "\\score {\n"
 notes += "  { "
+notes += "\\time 3/4\n"
+notes += "\\tempo 4. = 96\n"
+theme = ""
 for i in 'Nosferatu'.lower():
     o = ord(i) - ord('a') # get number in alphabet
     o  %= 8 # get back to a - h
     c = chr(o + ord('a')) # get char again
-    notes += c
-    notes += " "
+    theme += c
+    theme += " "
+for _ in range(4):
+    notes += theme
 notes += " }\n"
-notes += "  \\midi {}"
+notes += "  \\midi {}\n"
 notes += "}\n"
 file.write(notes)
