c40c6f2b494b3ceb98e0d59dbc211c347ebbadaa
Gon Solo <gonsolo@gmail.com>
2025-03-21 02:29:18 +0100

    Saxophone.

--- create.py
+++ create.py
@@ -2,6 +2,10 @@ file = open("nosferatu.ly", "w")
 
 notes = "\\version \"2.24.4\"\n"
 notes += "\\score {\n"
+
+notes += "\\new Staff {\n"
+notes += "  \\set Staff.midiInstrument = \"tenor sax\"\n"
+
 notes += "  { "
 notes += "\\time 3/4\n"
 notes += "\\tempo 4. = 96\n"
@@ -15,6 +19,9 @@ for i in 'Nosferatu'.lower():
 for _ in range(4):
     notes += theme
 notes += " }\n"
+
+notes += " }\n" # Staff
+
 notes += "  \\midi {}\n"
 notes += "}\n"
 file.write(notes)
